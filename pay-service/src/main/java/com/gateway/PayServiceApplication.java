007e75fcc9fc9f9be6658c9a5f9da18e87373cc2
guoyg <@>
2021-12-15 09:36:53 +0800

    修改类名

--- pay-service/src/main/java/com/gateway/PayServiceApplication.java
+++ pay-service/src/main/java/com/gateway/PayServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.gateway.mapper"})
 public class PayServiceApplication {
 
     public static void main(String[] args) {
